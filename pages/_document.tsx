1fef355e02cd8d369e37a8d1d4d115b09a7e411f
Thiago <thiagosalaberry99@gmail.com>
2024-02-16 16:56:11 -0300

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+        <title>Teoxys Shop</title>
       </Head>
       <body>
         <Main />
